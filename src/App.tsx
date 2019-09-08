6f0f386108e2cc5a9a6959a771415467ea0ea925
GabCh <gabcha25@gmail.com>
2019-09-08 14:35:32 -0400

    Add side menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 import sketch from './sketches/sketch';
 import './App.css';
+import SideMenu from "./components/SideMenu";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <P5Wrapper sketch={sketch}/>
+      <h1>Hello, World!</h1>
+      <SideMenu/>
     </div>
   );
 };
